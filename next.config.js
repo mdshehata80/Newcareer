9ef949f25e0220a4065fdfa9f2da78d032b3e980
Mohamed Shehata <mdshehata80@gmail.com>
2025-06-29 20:17:47 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+<<<<<<<< HEAD:next.config.js
+========
+// This file is deprecated. Please use next.config.js instead.
+// Keeping it to avoid breaking changes, but it is not used by Next.js.
+
+>>>>>>>> 84be54b3 (The NextJS app server is having trouble starting. Please identify what c):next.config.ts
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
